59ee713626993542aceed5d1c959facdbbd6749b
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-07-26 11:33:13 +0300

    Edits HW

--- src/index.js
+++ src/index.js
@@ -1,60 +1,76 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from "slim-select";
 
-const breedSelect = document.querySelector(".breed-select");
+
+const breedSelectElement = document.querySelector(".breed-select");
 const catInfoDiv = document.querySelector(".cat-info");
-const loader = document.querySelector(".loader");
+const loader = document.querySelector(".loader")
 
-breedSelect.classList.add("hidden");
+breedSelectElement.classList.add("hidden");
 
-fetchBreeds()
-  .then((response) => {
-    if (response) {
-        breedsList(response)
-      loader.classList.add("hidden");
-      breedSelect.classList.remove("hidden");
-    }
-  })
-  .catch((err) => {
-    loader.classList.add("hidden");
-    Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    console.error("Oops! Something went wrong! Try reloading the page!", err);
+function displayCatInfo(catData) {
+  const markup = catData
+  .map(({breeds:[{temperament,description,name}],url})=>{
+    return `
+       <img class="cat-image" src="${url}" alt="Cat Image">
+       <div class="cat-text">
+         <p><strong>Breed:</strong> ${name}</p>
+         <p><strong>Description:</strong> ${description}</p>
+         <p><strong>Temperament:</strong> ${temperament}</p>
+       </div>
+     `
+  }).join("")
+  catInfoDiv.insertAdjacentHTML('beforeend',markup)
+}
+
+function handleBreedSelectError(error) {
+  loader.classList.add("hidden")
+  Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  console.error(error)
+}
+
+function handleBreedSelectChange() {
+  const selectedBreedId = breedSelectElement.value;
+  fetchCatByBreed(selectedBreedId)
+    .then((catData) => {
+      if (catData.length === 0){
+        catInfoDiv.innerHTML = "";
+        Notify.failure('Oops! Something went wrong! Try reloading the page!')
+      } else {
+        catInfoDiv.innerHTML = "";
+        displayCatInfo(catData)
+      }
+      })
+    .catch(handleBreedSelectError);
+}
+
+function fillBreedSelect(breeds) {
+  const breedOptions = breeds.map(({name,id}) => ({ text: name, value: id }));
+  const breedSelect = new SlimSelect({
+    select: ".breed-select",
+    placeholder: "Select a breed",
+    data: breedOptions,
   });
 
-function breedsList(response) {
-const markup = response
-.map(({name,id}) => {
-  return `<option value="${id}">${name}</option>`;
-})
-.join("");
-breedSelect.insertAdjacentHTML('beforeend',markup)
+  breedSelectElement.addEventListener("change", handleBreedSelectChange);
 }
 
-breedSelect.addEventListener('change', onChange);
-
-function onChange() {
-  fetchCatByBreed(breedSelect.value)
-    .then((catInfo) => {
-        
-      renderCatInfo(catInfo);
-    })
-    .catch((err) => {
-        Notify.failure('Oops! Something went wrong! Try reloading the page!');
-        console.error("Oops! Something went wrong! Try reloading the page!", err);
-        });
+function initializePage() {
+  fetchBreeds()
+    .then((breeds) => {
+      if (breeds){
+        loader.classList.add("hidden")
+        fillBreedSelect(breeds)
+      }
+      })
+    .catch((error) => {
+      loader.classList.add("hidden")
+      Notify.failure('Oops! Something went wrong! Try reloading the page!')
+      console.error(error)
+    }
+    );
 }
 
-function renderCatInfo(catInfo) {
-  let markup = catInfo.map(({ breeds: [{ temperament, description, name }], url }) => {
-    return `
-      <img class="cat-image" src="${url}" alt="Котик">
-      <div class="cat-text">
-      <h2>${name}</h2>
-      <p><b>Опис:</b> ${description}</p>
-      <p><b>Темперамент:</b> ${temperament}</p>
-      </div>
-    `;
-  }).join("");
-  catInfoDiv.innerHTML = markup;
-}
+initializePage();
 
